5c1f8cdc25d3d9a4a10cac88cb22bd9b68fdb88d
RoshanCS <roshan.cs790@gmail.com>
2020-11-06 14:13:18 +0530

    updated

---
+++ netw.py
@@ -0,0 +1,19 @@
+from urllib.request import urlopen
+import urllib.error
+
+
+response  = urlopen('http://www.debian.org')
+
+#print(response.readline())
+#print(response.url)
+#print(response.read(50))
+#print(response.status)
+#print(response.read(1000).decode('utf-8'))
+print(response.getheaders())
+'''
+try:
+    response2 = urlopen('http://debian.org/halo.txt')
+except urllib.error.URLError as e:
+    print(e)
+'''
+
