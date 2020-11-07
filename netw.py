0d1cb33a3355533a6c05eb02ef8635db3ba9cc3b
RoshanCS <roshan.cs790@gmail.com>
2020-11-07 15:53:20 +0530

    Update netw.py

--- netw.py
+++ netw.py
@@ -4,12 +4,12 @@ import urllib.error
 
 response  = urlopen('http://www.debian.org')
 
-#print(response.readline())
+print(response.readline())
 #print(response.url)
 #print(response.read(50))
 #print(response.status)
 #print(response.read(1000).decode('utf-8'))
-print(response.getheaders())
+#print(response.getheaders())
 '''
 try:
     response2 = urlopen('http://debian.org/halo.txt')
